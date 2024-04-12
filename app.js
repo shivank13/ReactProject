2baa10f8f26ad0b5c7eb113c543c7eb4927dc8c2
shivanksharma4 <shivanksharma4@deloitte.com>
2024-04-13 03:25:53 +0530

    dependencies added

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+import React from "react"
+import  ReactDOM  from "react-dom/client";
 const heading = React.createElement("h1", {}, "Hello react");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
