f184c92a6ea66d8a2d4e675111dde7bb1c867afc
shivanksharma4 <shivanksharma4@deloitte.com>
2024-04-13 18:25:48 +0530

    functional component

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-import React from "react"
-import  ReactDOM  from "react-dom/client";
-const heading = React.createElement("h1", {}, "Hello react");
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const Title = () => <h1> Hello title</h1>;
+
+const Heading = () => (
+  <div>
+    <Title />
+    <h1>Hello React</h1>
+  </div>
+);
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(<Heading/>);
